f0717d5757ada3eb65cd7e704d36783c361d2554
Sid <siddharthr3535@gmail.com>
2025-03-19 00:32:19 -0400

    You are given an m x n grid rooms initialized with these three possible values.
    -1 A wall or an obstacle.
    0 A gate.
    INF Infinity means an empty room. We use the value 231 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
    Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.

---
+++ leet/WallsAndGates.java
@@ -0,0 +1,39 @@
+class Solution {
+  private void solve(int[][] rooms, int i, int j, Set<String> visited, Queue<int[]> q) {
+    if (i < 0 || j < 0 || i >= rooms.length || j >= rooms[0].length || rooms[i][j] == -1 || visited.contains(i + "," + j)) {
+      return;
+    }
+    visited.add(i + "," + j);
+    q.add(new int[]{i, j});
+  }
+
+  public void wallsAndGates(int[][] rooms) {
+    Queue<int[]> q = new LinkedList<>();
+    Set<String> visited = new HashSet<>();
+
+    for (int i = 0; i < rooms.length; i++) {
+      for (int j = 0; j < rooms[i].length; j++) {
+        if (rooms[i][j] == 0) {
+          q.add(new int[]{i, j});
+          visited.add(i + "," + j);
+        }
+      }
+    }
+
+    int distance = 0;
+    while (!q.isEmpty()) {
+      int size = q.size();
+      for (int k = 0; k < size; k++) {
+        int[] cell = q.remove();
+        int i = cell[0], j = cell[1];
+        rooms[i][j] = distance;
+
+        solve(rooms, i + 1, j, visited, q);
+        solve(rooms, i - 1, j, visited, q);
+        solve(rooms, i, j + 1, visited, q);
+        solve(rooms, i, j - 1, visited, q);
+      }
+      distance++;
+    }
+  }
+}
