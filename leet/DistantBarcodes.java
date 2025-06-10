7a33e531c635acff5ed848aa3cf735a8a43d97bd
Sid <siddharthr3535@gmail.com>
2025-06-10 12:12:22 -0400

    In a warehouse, there is a row of barcodes, where the ith barcode is barcodes[i].
    Rearrange the barcodes so that no two adjacent barcodes are equal. You may return any answer, and it is guaranteed an answer exists.
    
    Example 1:
    
    Input: barcodes = [1,1,1,2,2,2]
    Output: [2,1,2,1,2,1]

---
+++ leet/DistantBarcodes.java
@@ -0,0 +1,33 @@
+class Solution {
+  public int[] rearrangeBarcodes(int[] barcodes) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i : barcodes){
+      map.put(i, map.getOrDefault(i, 0) + 1);
+    }
+    PriorityQueue<Integer> maxheap = new PriorityQueue<>((a,b)-> map.get(b) - map.get(a));
+    int [] result = new int[barcodes.length];
+    int index = 0;
+    maxheap.addAll(map.keySet());
+    while(!maxheap.isEmpty()){
+      int first = maxheap.poll();
+      int second = maxheap.isEmpty() ? - 1 : maxheap.poll();
+      map.put(first , map.get(first) - 1);
+      result[index++] = first;
+      if(second != -1){
+        result[index++] = second;
+        map.put(second , map.get(second) - 1);
+
+      }
+      if (map.get(first) > 0){
+        maxheap.offer(first);
+      }
+      if(second != -1){
+        if(map.get(second) > 0){
+          maxheap.offer(second);
+        }
+      }
+    }
+
+    return result;
+  }
+}
