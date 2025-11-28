31d642c4afff923b34062920baf16b4832b4c69f
Sid <siddharthr3535@gmail.com>
2025-11-27 22:53:13 -0500

    LC 3297, 2 pointers, most optimal(?)

--- leet/CountSubstringsThatCanBeRearranged.py
+++ leet/CountSubstringsThatCanBeRearranged.py
@@ -1,28 +1,25 @@
 from typing import Counter
-
-
 class Solution:
     def validSubstringCount(self, word1: str, word2: str) -> int:
         i = 0
         j = 0
         d1= Counter(word2)
-        d = {}
+        d = Counter()
         result = 0
+        current = 0
+        l = len(word1)
+        final = len(d1)
         while j < len(word1):
-            if word1[j] not in d:
-                d[word1[j]] = 1
-            else:
-                d[word1[j]] += 1
-            valid = True
-            while valid:
-                for k in d1:
-                    if k not in d or d[k] < d1[k]:
-                        valid = False
-                        break
-                if valid == True:
-                    result += len(word1) - j 
-                    d[word1[i]] -= 1
-                    i += 1
+            d[word1[j]] += 1
+            if word1[j] in d1 and d[word1[j]] == d1[word1[j]]:
+                current += 1
+            while current == final:
+                result += l - j
+                if word1[i] in d1 and d[word1[i]] == d1[word1[i]]:
+                    current -= 1
+                d[word1[i]] -= 1
+                i += 1
+                    
             j += 1
         return result
             
