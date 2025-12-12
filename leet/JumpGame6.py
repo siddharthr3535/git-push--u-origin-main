610a004b57b617f9d28be7822d6f011fec7c58b9
Sid <siddharthr3535@gmail.com>
2025-12-12 13:24:27 -0500

    LC 1696

---
+++ leet/JumpGame6.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        heap = []
+        heapq.heappush(heap, [-nums[0],0])
+        result = nums[0]
+        for i in range(1,len(nums)):
+
+            while i - heap[0][1] > k:
+                heapq.heappop(heap)
+            value, index = heap[0]
+            result = -value + nums[i]
+            heapq.heappush(heap, [-result , i])
+
+        return result
+
+
+
